e83afe11e37454d25cf8dd8b91f5d48c29ae5170
gela pipia <gelapipia10@gmail.com>
2023-12-15 14:01:00 +0400

    uploading server side files, main entry file index.ts

---
+++ server/src/index.ts
@@ -0,0 +1,50 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user";
+import productRouter from "./routes/productRoute";
+import "dotenv/config"
+
+const app = express();
+
+const port = process.env.PORT
+
+
+//              Middlewares:
+
+// for parse incoming requests in json format
+app.use(express.json());
+
+// for connection through the client 
+app.use(cors());
+
+
+
+// for /user endpoint routes
+app.use("/user", userRouter);
+
+//fpr /product endpoint routes
+app.use("/product", productRouter);
+
+
+
+
+//      Connecting to DB:
+mongoose.connect(process.env.DB_URI)
+.then(() => {
+    console.log("Connected to Database")
+})
+.catch(err => {
+    console.log("NOT connected to database", err);
+})
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>hello world</h1>")
+})
+
+app.listen(port, () => {
+    console.log(`listening the port ${port}`)
+})
