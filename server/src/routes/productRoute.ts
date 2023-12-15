de8f8b60177b855f5edc1cb1db7e4b716389809a
gela pipia <gelapipia10@gmail.com>
2023-12-16 00:30:48 +0400

    added checkout route for /checkout API endpoit

--- server/src/routes/productRoute.ts
+++ server/src/routes/productRoute.ts
@@ -1,12 +1,13 @@
 import express from "express";
-import { addNewProduct, getProducts } from "../controllers/productController";
+import { addNewProduct, getProducts, checkoutProduct } from "../controllers/productController";
+import { verifyToken } from "../controllers/user";
 
 
 const productRouter = express.Router();
 
 productRouter.get("/getproducts", getProducts)
 productRouter.post("/newproduct", addNewProduct)
-
+productRouter.post("/checkout", verifyToken, checkoutProduct)
 
 
 export default productRouter;
