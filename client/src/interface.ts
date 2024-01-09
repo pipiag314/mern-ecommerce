c9cecf00b671cc9afb3d07ba30acfe28b989ddf0
gela pipia <gelapipia10@gmail.com>
2024-01-10 00:21:40 +0400

    added: interface.ts for types

---
+++ client/src/interface.ts
@@ -0,0 +1,9 @@
+export interface ProductInterface {
+    title: string;
+    quantity: number;
+    price: number;
+    description: string;
+    _id: string;
+    image: string;
+    category: string;
+}
